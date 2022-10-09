265f4d80c6398dae7c97875e4db76a1ea3750ee7
MinhNguyenDuc <nguyenducminh24101998@gmail.com>
2022-10-09 11:08:47 +0100

    Chapter 4

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"snippetbox/pkg/models"
 	"strconv"
 	"text/template"
 )
@@ -12,6 +14,17 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		app.notFound(w)
 		return
+	}
+
+	snippets, err := app.snippets.Latest()
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
 
 	files := []string{
@@ -44,7 +57,18 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Create new snippet"))
+	title := "0 snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 
 }
 
@@ -57,5 +81,18 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Displaying snippet: %v", id)
+
+	s, err := app.snippets.Get(id)
+
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+			return
+		} else {
+			app.serverError(w, err)
+			return
+		}
+	}
+
+	fmt.Fprintf(w, "Displaying snippet: %v", s)
 }
---
+++ test.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := 1
+
+	fmt.Print(s)
+	fmt.Print(&s)
+}
